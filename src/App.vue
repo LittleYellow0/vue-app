6eeb78d789dbca56d8bf3717c31d5bb290e03868
fizz <441980627@qq.com>
2017-08-02 21:37:09 +0800

    修改

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<transition name="router-fade" mode="out-in">
+			<router-view></router-view>
+		</transition>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="less">
+	.router-fade-enter-active,
+	.router-fade-leave-active {
+		transition: all .2s ease;
+	}
+	
+	.router-fade-enter,
+	.router-fade-leave-to {
+		transform: translateX(15px);
+		opacity: 0;
+	}
+</style>
